3039f7a46784c6bbc24899e51d342fb02ed80b51
songa <songacedrick111@gmail.com>
2023-09-27 23:54:14 +0300

    first commit

---
+++ src/main/java/SuccessServlet.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/SuccessServlet")
+public class SuccessServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SuccessServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			PrintWriter pw = response.getWriter();
+			String s = request.getParameter("fname");
+			pw.println("<div style=\"background-color: #f0f0f0; padding: 10px; border-radius: 5px; text-align: center;\">");
+			pw.println("<h2 style=\"color: green;\">Hello, You have successfully registered, " + s + "!</h2>");
+			pw.println("</div>");
+			pw.close();
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
